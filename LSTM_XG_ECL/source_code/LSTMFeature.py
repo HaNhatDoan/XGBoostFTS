809675b6049cbcf07a8af781597e434953c8aa9f
Hà Nhật Đoan <160616308+HaNhatDoan@users.noreply.github.com>
2025-07-22 10:44:49 +0700

    new

--- LSTM_XG_ECL/source_code/LSTMFeature.py
+++ LSTM_XG_ECL/source_code/LSTMFeature.py
@@ -1,10 +1,15 @@
-import torch
-from torch import nn
+import torch 
+import torch.nn as nn
 
 class LSTMFeatureExtractor(nn.Module):
-    def __init__(self, input_size, hidden_size=64, num_layers=2):
-        super(LSTMFeatureExtractor, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+    def __init__(self, input_size, hidden_size=128, num_layers=2, dropout=0.2):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=input_size,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            batch_first=True,
+                            dropout=dropout)
+        self.output = nn.Linear(hidden_size, 256)  # tăng vector đặc trưng
     def forward(self, x):
-        out, _ = self.lstm(x)
-        return out[:, -1, :]
+        _, (h_n, _) = self.lstm(x)
+        return self.output(h_n[-1])
